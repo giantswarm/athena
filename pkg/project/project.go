cf4cfa2e44904e1f01ad14a3644064350a6bf7bc
GitHub Action <action@github.com>
2021-04-12 09:33:31 +0000

    Bump version to 1.0.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "athena"
 	source      = "https://github.com/giantswarm/athena"
-	version     = "1.0.0"
+	version     = "1.0.1-dev"
 )
 
 func Description() string {
