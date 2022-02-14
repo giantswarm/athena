c535e6d846489f5570dad149c6e042914bd88b52
GitHub Action <action@github.com>
2022-02-14 13:50:58 +0000

    Bump version to 1.5.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "athena"
 	source      = "https://github.com/giantswarm/athena"
-	version     = "1.5.1"
+	version     = "1.5.2-dev"
 )
 
 func Description() string {
