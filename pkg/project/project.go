241caae1e87c6c094f352704caa18c09fd0a750a
taylorbot <dev@giantswarm.io>
2023-07-13 14:09:24 +0000

    Bump version to 1.10.4-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "athena"
 	source      = "https://github.com/giantswarm/athena"
-	version     = "1.10.3"
+	version     = "1.10.4-dev"
 )
 
 func Description() string {
